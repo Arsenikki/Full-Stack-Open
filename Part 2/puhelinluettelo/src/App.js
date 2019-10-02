007f2fef8cb670abbddc16aa84fcda48cd18e04c
Arseni Leskinen <arsenileskinen@gmail.com>
2019-10-02 17:57:57 +0300

    Filtering added

--- Part 2/puhelinluettelo/src/App.js
+++ Part 2/puhelinluettelo/src/App.js
@@ -2,12 +2,18 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '1234' , id: 1 }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [ newName, setNewName ] = useState(
     ''
   )
   const [ newNumber, setNewNumber ] = useState(
+    ''
+  )
+  const [ newFilter, setNewFilter ] = useState(
     ''
   )
 
@@ -37,17 +43,20 @@ const App = () => {
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
-  const rows = () => persons.map(person =>
-    // console.log("pepet:",persons) ||c
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const personsToBeShown = persons.filter(person => person.name.toLowerCase().includes(newFilter))
+
+  const rows = () => personsToBeShown.map(person =>
     <Person
       key={person.name}
       person={person}
@@ -56,13 +65,17 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      filter shown with
+      <input
+          value={newFilter}
+          onChange={handleFilterChange}
+        />
+      <h2>Add a new</h2>
       <form onSubmit = {addPerson}>
         <div> name: <input value = {newName} onChange = {handleNameChange} /> </div>
-        <div>number: <input value = {newNumber} onChange = {handleNumberChange} /> </div>
-        <div>
-          <button type = "submit">add</button>
-        </div>
+        <div> number: <input value = {newNumber} onChange = {handleNumberChange} /> </div>
+        <div> <button type = "submit">add</button> </div>
       </form>
       <h2>Numbers</h2>
       {rows()}
