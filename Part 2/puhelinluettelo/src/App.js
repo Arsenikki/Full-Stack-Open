562ef080309308d96978a975ab5baf41fba93fa4
Arseni Leskinen <arsenileskinen@gmail.com>
2019-10-03 17:54:34 +0300

    2.10 done, Separated parts into components

--- Part 2/puhelinluettelo/src/App.js
+++ Part 2/puhelinluettelo/src/App.js
@@ -1,91 +1,80 @@
 import React, { useState } from 'react'
+import Filter from './components/Filter'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [ newName, setNewName ] = useState(
-    ''
-  )
-  const [ newNumber, setNewNumber ] = useState(
-    ''
-  )
-  const [ newFilter, setNewFilter ] = useState(
-    ''
-  )
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    ])
+    const [newName, setNewName] = useState(
+        ''
+    )
+    const [newNumber, setNewNumber] = useState(
+        ''
+    )
+    const [newFilter, setNewFilter] = useState(
+        ''
+    )
 
-  const addPerson = (event) => {
-    event.preventDefault()
-    let nameIsUnique = CheckForDuplicates(newName)
-    if (nameIsUnique) {
-      const nameObject = {
-        name: newName,
-        number: newNumber,
-        id: persons.length + 1,
-      }
-      setPersons(persons.concat(nameObject))
+    const addPerson = (event) => {
+        event.preventDefault()
+        let nameIsUnique = CheckForDuplicates(newName)
+        if (nameIsUnique) {
+            const nameObject = {
+                name: newName,
+                number: newNumber,
+                id: persons.length + 1,
+            }
+            setPersons(persons.concat(nameObject))
+        }
+        setNewName('')
+        setNewNumber('')
     }
-    setNewName('')
-    setNewNumber('')
-  }
 
-  const CheckForDuplicates = (newName) => {
-    for(var i = 0; i < persons.length; i++) {
-      if (persons[i].name === newName) {
-        window.alert(`${newName} is already added to phonebook`);
-        return false;
-      }
+    const CheckForDuplicates = (newName) => {
+        for (var i = 0; i < persons.length; i++) {
+            if (persons[i].name === newName) {
+                window.alert(`${newName} is already added to phonebook`);
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
-  }
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
 
-  const handleFilterChange = (event) => {
-    setNewFilter(event.target.value)
-  }
+    const handleFilterChange = (event) => {
+        setNewFilter(event.target.value)
+    }
 
-  const personsToBeShown = persons.filter(person => person.name.toLowerCase().includes(newFilter))
+    const personsToBeShown = persons.filter(person => person.name.toLowerCase().includes(newFilter))
 
-  const rows = () => personsToBeShown.map(person =>
-    <Person
-      key={person.name}
-      person={person}
-    />
-  )
-
-  return (
-    <div>
-      <h1>Phonebook</h1>
-      filter shown with
-      <input
-          value={newFilter}
-          onChange={handleFilterChange}
-        />
-      <h2>Add a new</h2>
-      <form onSubmit = {addPerson}>
-        <div> name: <input value = {newName} onChange = {handleNameChange} /> </div>
-        <div> number: <input value = {newNumber} onChange = {handleNumberChange} /> </div>
-        <div> <button type = "submit">add</button> </div>
-      </form>
-      <h2>Numbers</h2>
-      {rows()}
-    </div>
-  )
+    return (
+        <div>
+            <h1>Phonebook</h1>
+            filter shown with
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
+            <h2>Add a new</h2>
+            <PersonForm
+                onSubmit={addPerson}
+                newName={newName}
+                handleNameChange={handleNameChange}
+                newNumber={newNumber}
+                handleNumberChange={handleNumberChange} />
+            <h2>Numbers</h2>
+            <Persons persons={personsToBeShown} />
+        </div>
+    )
 }
 
-const Person = ({ person }) => {
-  return (
-    <li key = {person.name}> {person.name} {person.number}</li>
-  )
-}
 export default App
