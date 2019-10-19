0eee70a73a1b0ba58b1bfa863cc14b3167e13b50
Arseni Leskinen <arseni.leskinen@fi.abb.com>
2019-10-19 21:19:55 +0300

    3.16 done,. Added middleware for error handling

--- Part 3/puhelinluettelo-backend/index.js
+++ Part 3/puhelinluettelo-backend/index.js
@@ -26,19 +26,20 @@ app.get('/info', (req, res) => {
     res.end()
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person.toJSON())
       } else {
-        response.status(404).end()
+        response.status(204).end()
       }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    console.log("boodyy", body)
     if (!body.name) {
         return response.status(400).json({ 
           error: 'name missing' 
@@ -62,13 +63,31 @@ app.post('/api/persons', (request, response) => {
     }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
     .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
+  
+const errorHandler = (error, request, response, next) => {
+console.error(error.message)
+
+if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+}
+
+next(error)
+}
+  
+  app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
