479e15bcf0e0b1a174c6664c2bef95feb3a45ade
Arseni Leskinen <arseni.leskinen@fi.abb.com>
2019-11-08 01:14:21 +0200

    5.3 done, new blogs can be posted from frontend. Added id to login response in backend to get correct user for new blogs.

--- Part 5/bloglist-frontend/src/App.js
+++ Part 5/bloglist-frontend/src/App.js
@@ -8,6 +8,9 @@ const App = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
+  const [newAuthor, setNewAuthor] = useState("");
+  const [newTitle, setNewTitle] = useState("");
+  const [newUrl, setNewUrl] = useState("");
 
   useEffect(() => {
     blogsService.getAll().then(initialBlogs => setBlogs(initialBlogs));
@@ -34,10 +37,34 @@ const App = () => {
       setUser(user);
       setUsername("");
       setPassword("");
+      console.log(user);
       console.log("login succeeded");
     } catch (exception) {
       console.log("error at login");
     }
+  };
+
+  const handleTitleChange = event => setNewTitle(event.target.value);
+  const handleAuthorChange = event => setNewAuthor(event.target.value);
+  const handleUrlChange = event => setNewUrl(event.target.value);
+
+  const handleSubmit = event => {
+    alert("A name was submitted: ");
+    event.preventDefault();
+    blogsService
+      .create({
+        title: newTitle,
+        author: newAuthor,
+        url: newUrl,
+        likes: 0,
+        userId: user.id
+      })
+      .then(createdBlog => {
+        setBlogs(blogs.concat(createdBlog));
+        setNewAuthor("");
+        setNewTitle("");
+        setNewUrl("");
+      });
   };
 
   const loginForm = () => (
@@ -72,6 +99,29 @@ const App = () => {
       <h2>blogs</h2>
       <h4> {user.name} logged in</h4>
       <button onClick={() => window.localStorage.clear()}>logout</button>
+      <h2>create new blog</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            title:
+            <input value={newTitle} onChange={handleTitleChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            author:
+            <input value={newAuthor} onChange={handleAuthorChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            url:
+            <input value={newUrl} onChange={handleUrlChange} />
+          </label>
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+
       {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
       ))}
